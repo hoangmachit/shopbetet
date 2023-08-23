f9ecff2c664cd424b11793abe0b7d883fe00608b
AGL_Mạch Văn Hoàng (ホアン) <81142674+machvanhoang@users.noreply.github.com>
2023-08-23 14:30:42 +0700

    Product detail

--- js/app.js
+++ js/app.js
@@ -31,3 +31,19 @@ owl__product__puerchased.owlCarousel({
     dots: false,
     items: 3,
 });
+
+var owl__gallery = $('.owl__gallery');
+owl__gallery.owlCarousel({
+    margin: 10,
+    loop: true,
+    nav: true,
+    dots: false,
+    items: 6,
+});
+$(document).on('click', '.owl__gallery .owl-item', function () {
+    const index = $(this).index();
+    const image = $(this).find('.product__thumb--item').data('image');
+    $('.owl__gallery').trigger('owl.goTo', index);
+    $('.product__detail--left .thumbs__main picture img').attr('src', image);
+});
+
